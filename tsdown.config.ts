0fe4ef659aa20744c15d27d5d6ee22785672914d
johan <johan.vlassak@gmail.com>
2025-10-28 15:16:35 -0700

    added the files

--- tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import type { UserConfig, UserConfigFn } from 'tsdown/config'
-import { defineConfig } from 'tsdown/config'
-
-const config: UserConfig | UserConfigFn = defineConfig({
-  entry: ['src/index.ts'],
-  dts: true,
-})
-
-export default config
